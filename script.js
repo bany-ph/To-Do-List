9823241f2e1c06a237e3b235d171e95373566656
bany-ph <brisbanystudy@gmail.com>
2025-06-20 13:26:08 -0500

    update

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const add = document.getElementById("add-task");
 
-
+const containerList = document.querySelector(".task-list")
 add.addEventListener("click", function(event){
     
     event.preventDefault();
-    let p = document.createElement("p");
+    
+
+
+    let div = document.createElement("div");
     const taskName = document.querySelector("#task-name").value;
 
     if(taskName == '') { //check if the input is empty
@@ -12,22 +15,45 @@ add.addEventListener("click", function(event){
         return // stops the event
     }
 
-    /*create and show the task */
-    p.textContent = taskName; 
-    p.className = "task"
-    document.querySelector(".task-list").appendChild(p);
+    /*create and show the task in <p> */
+    const p = document.createElement("p")
+    p.textContent = taskName;
+    div.appendChild(p);
+    containerList.appendChild(div);
 
-    
+
+  
+
     /* create and show the delete icon */
     const icon = document.createElement("i");
-    icon.className = "fa-solid fa-trash"
-    p.appendChild(icon) 
+    icon.className = "fa-solid fa-trash icon"
+    div.appendChild(icon) 
     
+
+      /* check box */
+    /*   const check = document.createElement("i");
+      check.className = "icon-check";
+      div.insertBefore(check, p);
+      
+   */
+
+
    /* delete task */
-    icon.onclick = function () {
-        p.remove();
-    }
+    
 
 
     document.getElementById("task-name").value = "" // clear the input
 })
+
+document.body.addEventListener("click", nose('icon', containerList))
+
+function nose(classs, contenedor, event) {
+    if (event.target.classList.contains(classs)) {
+        const li = event.target.parentElement;
+        contenedor.removeChild(li);
+    }
+}
+
+// icon.onclick = function () {
+//         div.remove();
+//     }
