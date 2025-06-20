a0c5a1b066c5c61432213da53bed47eed0144bd3
bany-ph <brisbanystudy@gmail.com>
2025-06-20 07:41:43 -0500

    feat: add delete function

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 const add = document.getElementById("add-task");
 
 
-add.addEventListener("click", () =>{
-    /* add task */
+add.addEventListener("click", function(event){
+    
+    event.preventDefault();
     let p = document.createElement("p");
-    if(document.getElementById("task-name").value.trim() === '') { //check if the input is empty
+    const taskName = document.querySelector("#task-name").value;
+
+    if(taskName == '') { //check if the input is empty
         alert("put the name of the task")
         return // stops the event
     }
-    /*create and show the element */
-    p.textContent = document.getElementById("task-name").value
+
+    /*create and show the task */
+    p.textContent = taskName; 
     p.className = "task"
     document.querySelector(".task-list").appendChild(p);
 
     
-
-
-
-
-
-
-
-
-
-
+    /* create and show the delete icon */
+    const icon = document.createElement("i");
+    icon.className = "fa-solid fa-trash"
+    p.appendChild(icon) 
+    
+   /* delete task */
+    icon.onclick = function () {
+        p.remove();
+    }
 
 
     document.getElementById("task-name").value = "" // clear the input
